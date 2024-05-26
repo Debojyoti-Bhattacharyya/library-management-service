e3f80a233fb175796c1ffdb7a2d4986b533f6702
Debojyoti-Bhattacharyya <debojyotibhattacharyya@gmail.com>
2024-05-27 02:27:53 +0530

    Added Login Page

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,13 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import BookList from "./components/BookList";
+import Login from "./components/Login";
 
 function App() {
 	return (
 		<Router>
 			<Routes>
+				<Route path="/login" element={<Login />} />
 				<Route exact path="/books" element={<BookList />} />
 			</Routes>
 		</Router>
