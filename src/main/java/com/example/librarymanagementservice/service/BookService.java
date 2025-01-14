7197d9114bdc55943884df666927ddaab18d0126
Debojyoti-Bhattacharyya <debojyotibhattacharyya@gmail.com>
2025-01-15 00:05:50 +0530

    Replaced getAllBooks last line with method reference

--- src/main/java/com/example/librarymanagementservice/service/BookService.java
+++ src/main/java/com/example/librarymanagementservice/service/BookService.java
@@ -25,7 +25,7 @@ public class BookService {
         return bookRepository
                 .findAll()
                 .collectList()
-                .map(books -> new BookListResponse(books));
+                .map(BookListResponse::new);
     }
 
     public Mono<Book> getBookById(String id) {
